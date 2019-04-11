b7fe4ceb3f684565a7bcbc7ecd9cae993d759efe
Alexander Danilchenko <39950413+OlexanderD@users.noreply.github.com>
2019-04-11 14:50:03 +0300

    Update polynom generator.cpp

--- polynom generator.cpp
+++ polynom generator.cpp
@@ -34,7 +34,6 @@ int main() {
 size_t randomGenerator(size_t min, size_t max) {
 	std::mt19937 rng;
 	rng.seed(std::random_device()());
-	//rng.seed(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 	std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
 	return dist(rng);
 }
